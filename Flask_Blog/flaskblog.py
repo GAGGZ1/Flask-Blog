0dd0238344396d9d0652c691c7a47ed762375b48
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2024-11-22 00:01:36 +0530

    added files

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,15 +1,31 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 
 app = Flask(__name__)
+
+posts=[
+    {
+        'author':'corey schafer',
+        'title':'blog post 1',
+        'content':'First post Content',
+        'date_posted':'April 20, 2018'
+    },
+    {
+        'author':'Jane Smith',
+        'title':'blog post 2',
+        'content':'Second post Content',
+        'date_posted':'May 15, 2018'
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title='About')
 
 
 if __name__ == '__main__':
