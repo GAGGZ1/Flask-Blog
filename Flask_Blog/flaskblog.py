9162e20c8f196a6d3b2a7fee7e7cf94d9ddbb050
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2024-11-22 16:45:19 +0530

    Added files

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask,render_template,url_for
-
+from flask import Flask,render_template,url_for,flash,redirect
+from forms import RegistrationForm,LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] ='cc1954a0b2fa7878e90d95eb1bfbcae8'
 
 posts=[
     {
@@ -26,6 +28,20 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title='About')
+
+@app.route("/register", methods=["POST",'get'])
+def register():
+    form =RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='Register',form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
 
 
 if __name__ == '__main__':
