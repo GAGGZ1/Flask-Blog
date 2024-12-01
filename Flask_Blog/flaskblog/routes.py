33d2aeb1e9369ece6d03b39ced2548a3d98b3ada
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2024-12-01 23:32:24 +0530

    Added Feature- Create, Update, and Delete Posts

--- Flask_Blog/flaskblog/routes.py
+++ Flask_Blog/flaskblog/routes.py
@@ -2,7 +2,7 @@ import os
 import secrets
 from PIL import Image
 from flask import render_template,url_for,flash,redirect,request
-from flaskblog.forms import RegistrationForm,LoginForm,UpdateAccountForm
+from flaskblog.forms import RegistrationForm,LoginForm,UpdateAccountForm, PostForm
 from flaskblog.models import User,Post
 from flaskblog import app,db,bcrypt
 from flask_login import  login_user,current_user,logout_user, login_required
@@ -109,3 +109,13 @@ def account():
     image_file=url_for('static',filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account',
                            image_file=image_file,form=form)
+
+
+@app.route('/post/new',methods=["POST",'get'])
+@login_required
+def new_post():
+    form=PostForm()
+    if form.validate_on_submit():
+        flash('Your Post has been created!','success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post',form=form)
