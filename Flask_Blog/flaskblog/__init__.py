4c02e94fb77511d3da5541c8aa3fae3a4dcfb512
Gagan Chauhan <81284966+GAGGZ1@users.noreply.github.com>
2024-12-06 00:04:10 +0530

    Added Error Pages

--- Flask_Blog/flaskblog/__init__.py
+++ Flask_Blog/flaskblog/__init__.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -27,8 +26,10 @@ def create_app(config_class=Config):
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
